d8dc1b3dbb96d6f91b7447377c7dee380f36cb7c
Cangu <xmutizabal@gmail.com>
2025-02-05 21:20:37 -0300

    se incorpora login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import { redirect } from "next/navigation";
+
 
 export default function Home() {
-  redirect("/dashboard");
+
   return (
     <div>
-      hola mundo
+      landing page
     </div>
   );
 }
